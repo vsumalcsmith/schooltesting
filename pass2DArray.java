97f40486d4aeb6c3390d0563bba164080017a5b5
vsumalcsmith <44275927+vsumalcsmith@users.noreply.github.com>
2019-05-06 23:28:15 -0400

    Update pass2DArray.java

--- pass2DArray.java
+++ pass2DArray.java
@@ -2,10 +2,21 @@ import java.util.Scanner;
 
 
 public class 2D{
+  
+  private static int r, c;
+  
 
 
 
   public static void main(String [] args){
+    Scanner input = new Scanner(System.in);
+    
+    
+    System.out.println("Enter the number of rows");
+    r = input.nextInt();
+    
+    System.out.println("Enter the number of columns");
+    c = input.nextInt();
     
   }
   
@@ -14,11 +25,19 @@ public class 2D{
   
   Scanner input = new Scanner(System.in);
   int [][] x = new int [r][c];
+    
+    
+    
     for(int e = 0; e < x.length; e++){
       for(int t = 0; t < t[e].length; t++){
         x[e][t] = input.nextInt();
       }
     }
+    
+    
+    
+    
+    
   }
   
 
